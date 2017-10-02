d5adc342ec626227a9354c1513f9d4d2a8b91f39
Aaron Rosenberg <aarongrosenberg@gmail.com>
2017-10-02 12:27:46 -0400

    Test future time

---
+++ time_travel.rb
@@ -0,0 +1,20 @@
+#!/usr/bin/env ruby
+require 'benchmark'
+# Make sure you `gem install benchmark-ips`
+require 'benchmark/ips'
+require 'active_support/duration'
+
+benchmark_lambda = lambda do |x|
+  x.report('from_now') do # slightly faster
+    3.minutes.from_now
+  end
+
+  x.report('now + ') do
+    Time.current + 3.minutes
+  end
+
+  # uncomment if you want comparisons between them all
+  # x.compare!
+end
+
+Benchmark.ips(&benchmark_lambda); nil
