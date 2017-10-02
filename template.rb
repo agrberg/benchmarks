a68b012b3957ba9a77d3e7abb432368726829377
Aaron Rosenberg <aarongrosenberg@gmail.com>
2017-10-02 18:26:54 -0400

    Update template

--- template.rb
+++ template.rb
@@ -8,16 +8,13 @@ TIMES = [1, 16, 100, 1_000, 10_000]
 benchmark_lambda = lambda do |x|
   TIMES.each do |i|
     x.report("first case") do
-      puts 'first case'
     end
 
     x.report("2nd case") do
-      puts '2nd case'
     end
   end
 
-  # uncomment if you want comparisons between them all
-  # x.compare!
+  # x.compare! # uncomment if you want comparisons between them all
 end
 
 Benchmark.ips(&benchmark_lambda); nil
