acaf481a41e06af94948da544c658c3bd2bb78ed
Aaron Rosenberg <aarongrosenberg@gmail.com>
2017-12-05 19:08:29 -0500

    Update template

--- template.rb
+++ template.rb
@@ -7,10 +7,10 @@ TIMES = [1, 16, 100, 1_000, 10_000]
 
 benchmark_lambda = lambda do |x|
   TIMES.each do |i|
-    x.report("first case") do
+    x.report("FIRST_WAY - #{i}") do
     end
 
-    x.report("2nd case") do
+    x.report("SECOND_WAY - #{i}") do
     end
   end
 
