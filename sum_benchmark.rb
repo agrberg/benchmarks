5c8d60eb43e95bb7ddead0c273fc88107d47ed49
Aaron Rosenberg <aarongrosenberg@gmail.com>
2017-12-05 19:16:53 -0500

    update sum_benchmark

--- sum_benchmark.rb
+++ sum_benchmark.rb
@@ -1,15 +1,14 @@
-require 'ostruct'
 require 'benchmark'
 # Make sure you `gem install benchmark-ips`
 require 'benchmark/ips'
 
-TIMES = [1, 16, 100, 1_000, 10_000]
+TIMES = [0, 1, 2, 16, 100, 1_000, 10_000]
 
 benchmark_lambda = lambda do |x|
   TIMES.each do |num|
-    array = num.times.collect.to_a # goofy but it basically does {|i| i }
+    array = (1..num).to_a
 
-    x.report("manual #{num}") do
+    x.report("manual #{num}") do # fastest of the rest
       sum = 0
       array.each {|i| sum += i }
       sum
@@ -22,7 +21,13 @@ benchmark_lambda = lambda do |x|
     x.report("reduce &:+ #{num}") do
       array.reduce(&:+)
     end
+
+    x.report("sum - #{num}") do # so much freakin' faster
+      array.sum
+    end
   end
+
+  x.compare!
 end
 
 Benchmark.ips &benchmark_lambda
