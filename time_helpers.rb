0bebc4b60ada7876ed93e847eaf298a99d666439
Aaron Rosenberg <aarongrosenberg@gmail.com>
2021-05-18 17:52:10 -0500

    Update benchmarks for modern patterns

--- time_helpers.rb
+++ time_helpers.rb
@@ -2,15 +2,18 @@ require 'ostruct'
 require 'benchmark'
 # Make sure you `gem install benchmark-ips`
 require 'benchmark/ips'
+require 'active_support/core_ext/numeric/time'
 
 benchmark_lambda = lambda do |x|
-  x.report("24.hours.ago") do
+  x.report("24.hours.ago") do # SLOWER 2.2x O_o
     24.hours.ago
   end
 
   x.report("now - calculation") do
     Time.now.utc - 24 * 60 * 60
   end
+
+  x.compare!
 end
 
 Benchmark.ips &benchmark_lambda
--- time_travel.rb
+++ time_travel.rb
@@ -2,7 +2,7 @@
 require 'benchmark'
 # Make sure you `gem install benchmark-ips`
 require 'benchmark/ips'
-require 'active_support/duration'
+require 'active_support/core_ext/numeric/time'
 
 benchmark_lambda = lambda do |x|
   x.report('from_now') do # slightly faster
@@ -14,7 +14,7 @@ benchmark_lambda = lambda do |x|
   end
 
   # uncomment if you want comparisons between them all
-  # x.compare!
+  x.compare!
 end
 
 Benchmark.ips(&benchmark_lambda); nil
