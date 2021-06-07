5016cf5d88c2f92124e56f709e3082ad9f3cfeb3
Aaron Rosenberg <aarongrosenberg@gmail.com>
2021-06-06 20:07:29 -0500

    re-run and annotate non-sortable best

--- sort_reverse.rb
+++ sort_reverse.rb
@@ -3,22 +3,34 @@ require 'benchmark'
 # Make sure you `gem install benchmark-ips`
 require 'benchmark/ips'
 
-TIMES = [1, 16, 100, 1_000, 10_000]
+TIMES = [10, 100, 1_000]
 
 benchmark_lambda = lambda do |x|
   TIMES.each do |i|
     arr = Array.new(i) { rand(1_000) }
 
-    x.report("sort.reverse - #{i}") do # consistently faster on Ruby 3 and 2.5
+    x.report("#{i} - sort.reverse") do # consistently faster on Ruby 3 and 2.5
       arr.sort.reverse
     end
 
-    x.report("sort { b <=> a } - #{i}") do
+    x.report("#{i} - sort { b <=> a }") do
       arr.sort { |a, b| b <=> a }
     end
 
-    x.report("sort_by(&:-@) - #{i}") do
+    x.report("#{i} - sort_by(&:to_i).reverse") do
+      arr.sort_by(&:to_i).reverse
+    end
+
+    x.report("#{i} - sort_by{_1.to_i}.reverse") do
+      arr.sort_by { _1.to_i }.reverse
+    end
+
+    x.report("#{i} - sort_by(&:-@)") do
       arr.sort_by(&:-@)
+    end
+
+    x.report("#{i} - sort_by{-_1}") do # fastest for non-sortables (could depend on op in block) - more than &:proc
+      arr.sort_by { -_1 }
     end
   end
 
