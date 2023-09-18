e23da88f8330fbb0d017636939bb9a4331448a34
Aaron Rosenberg <aarongrosenberg@gmail.com>
2023-09-18 14:13:51 -0400

    splatting a hash return in a literal is faster than merging but merging two premade hashes is the fastest

---
+++ hash_merge_or_splat.rb
@@ -0,0 +1,31 @@
+#!/usr/bin/env ruby
+require 'benchmark'
+# Make sure you `gem install benchmark-ips`
+require 'benchmark/ips'
+
+TIMES = [1, 16, 100, 1_000, 10_000]
+
+benchmark_lambda = lambda do |x|
+  a = {one: 1, two: 2}
+  b = {three: 3, four: 4}
+
+  x.report("merge - 2") do # fastest — has some benefit from non-literal hash, splat - 2 has the outer hash and is the slowest
+    a.merge(b)
+  end
+
+  x.report("merge - literal") do
+    {one: 1, two: 2}.merge(b)
+  end
+
+  x.report("splat - 2") do
+    {**a, **b}
+  end
+
+  x.report("splat - literal") do # slightly faster than merge - literal
+    {one: 1, two: 2, **b}
+  end
+
+  x.compare! # uncomment if you want comparisons between them all
+end
+
+Benchmark.ips(&benchmark_lambda); nil
